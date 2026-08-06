3bc849562307c718a01dd2ba13f409501ee9b2de
sureshnarsingu63 <sureshnarsingu63@gmail.com>
2026-08-06 08:23:20 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character,Character> matchMap = Map.of(
+            ')', '(',
+            '}', '{',
+            ']', '['
+        );
+        for(char c: s.toCharArray()) {
+            if(matchMap.containsKey(c)) {
+                if (stack.isEmpty() || stack.pop() != matchMap.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
